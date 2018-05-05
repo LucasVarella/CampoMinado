3731fd0d9fecd2efa3a1e3d771b2602741c3fa96
Aluno <Aluno@DESKTOP-BSF5MOE>
2018-05-05 10:56:21 -0300

    Inicialização do projeto do Campo Minado

---
+++ src/CampoMinado/GridLayoutFrame.java
@@ -0,0 +1,15 @@
+package CampoMinado;
+import java.awt.event.ActionListener;
+import java.awt.GridLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+
+import javax.swing.JFrame;
+public class GridLayoutFrame extends JFrame implements ActionListener{
+    private JButton[]buttons; //array de botões
+    // private static final String[]names= {"","","","","","","","","","","","","","","","","","","","","","","","",""};
+    private boolean toogle = true;
+   
+  
+}
